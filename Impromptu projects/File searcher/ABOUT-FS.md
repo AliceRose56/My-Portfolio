cdeb30b4aacd4420f348afac4adf973f121c7525
AliceWonder56 <141330858+AliceWonder56@users.noreply.github.com>
2024-08-16 23:07:58 +0200

    file searcher project added
    Read ABOUT-FS.md

---
+++ Impromptu projects/File searcher/ABOUT-FS.md
@@ -0,0 +1,11 @@
+I am going off script in a lesson on node.js because I really wanted to try an idea I had for a faster solution to the lesson. Here's the premise:
+
+I am searching through files to find the secret word. The lesson would have you access each txt file one at a time, then manually change your code to use the new information you gleaned from each file, but I wanted to try an all-in-one-go approach to search each file in sequence to find the secret word with no script changes between.
+
+What I learned from this:
+
+1. About RegEx and how to use it to search through arrays/strings in JavaScript
+2. How to access filesystems with Node.js
+3. The importance and uses of Asyncronous programing
+
+As a bonus, I got more experience in troubleshooting and utilizing various documentations to implement my ideas. Also I had fun learning this and developing my skills ^w^
---
+++ Impromptu projects/File searcher/file-searcher.js
@@ -0,0 +1,29 @@
+const fs = require("fs"); //provides access to filesystem methods like readFile
+
+let secretWord = null; //declared variable intended to be updated while solution is found
+
+//error first callback function for asyncronous work
+let readFileCB = (err, data) => {
+  if (err) {
+    console.log(`Data not found: ${err}`);
+  } else {
+    let newData = data; //declared variable intended to be updated as the function loops
+    
+    //Code below searches a file for text that says ".txt" in a loop until file doesn't contain it
+    if (newData.includes(".txt") === true) { 
+      let stringData = newData.toString(); 
+      let firstIndex = stringData.search(/\w+\.txt/g); //searches for the index of a word that ends in '.txt'
+      let secondIndex = stringData.search(".txt");
+      let slicedString = stringData.slice(firstIndex, secondIndex);
+      newData = slicedString; //updates newData
+      fs.readFile(`./${newData}.txt`, "utf-8", readFileCB); //loops into the next file
+    } else { //if a file doesn't have ".txt", then it must have the secret word we're looking for!
+      let stringData = newData.toString(); 
+      let secretString = stringData.match(/("\w+")/g).toString().replace(/"/g, ''); //Checks the data for ""s around a word
+      secretWord = secretString; //solution found, thus updated
+      console.log(secretWord); //just checking everthing worked
+    }
+  }
+};
+
+fs.readFile("./fileOne.txt", "utf-8", readFileCB); //intializes the callback function
