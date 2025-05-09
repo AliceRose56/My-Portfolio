a7e256b7808b197a57abe254566a90593d8c7664
AliceWonder56 <141330858+AliceWonder56@users.noreply.github.com>
2025-05-09 13:59:53 +0200

    crlf

--- Challenges/Quote-API/Quote-API-my-solutionWIP/server.js
+++ Challenges/Quote-API/Quote-API-my-solutionWIP/server.js
@@ -8,3 +8,48 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+
+
+app.get('/api/quotes/random', (req, res , next) => {
+    res.send({
+        quote: getRandomElement(quotes)
+    });
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const person = req.query.person;
+    if (person) {
+        const filteredQuotes = quotes.filter(quote => quote.person === person);
+        res.send({
+            quotes: filteredQuotes
+        });
+    } else {
+        res.send({
+            quotes: quotes
+        });
+    };
+});
+
+app.post('/api/quotes', (req, res, next) => {
+    const quote = req.query.quote;
+    const person = req.query.person;
+    if (person && quote) {
+        const newQuote = {
+            quote: quote,
+            person: person
+        };
+
+        quotes.push(newQuote)
+        res.status(201).send({
+            quote: newQuote
+        });
+    } else {
+        res.status(400).send();
+    };
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+  });
